79f2dd4cb56ca65a5628380042de21d34e59fd18
dianakv16 <dianak90@gmail.com>
2017-03-12 11:59:13 +0000

    Trilateration algorithm completed

--- ParkingWithFlowParking.playground/Contents.swift
+++ ParkingWithFlowParking.playground/Contents.swift
@@ -1,23 +1,21 @@
 //: Playground - noun: a place where people can play
-
 import UIKit
-import Darwin
 
 //#assuming elevation = 0
-let PI = CDouble(3.1415926535897932384626433832795028841971693993751)
-let RADIANS = CDouble(180.00/PI)
+let PI = 3.1415926535897932384626433832795028841971693993751
+let RADIANS = PI/180
 
 
-let earthR = CDouble(6371.00)
-let LatA = CDouble(37.418436)
-let LonA = CDouble(-121.963477)
-let DistA = CDouble(0.265710701754)
-let LatB = CDouble(37.417243)
-let LonB = CDouble(-121.961889)
-let DistB = CDouble(0.234592423446)
-let LatC = CDouble(37.418692)
-let LonC = CDouble(-121.960194)
-let DistC = CDouble(0.0548954278262)
+let earthR = 6371.00
+let LatA = 37.418436
+let LonA = -121.963477
+let DistA = 0.265710701754
+let LatB = 37.417243
+let LonB = -121.961889
+let DistB = 0.234592423446
+let LatC = 37.418692
+let LonC = -121.960194
+let DistC = 0.0548954278262
 
 //#using authalic sphere
 //#if using an ellipsoid this step is slightly different
@@ -27,52 +25,79 @@ let DistC = CDouble(0.0548954278262)
 var xA = (earthR * (cos(RADIANS * LatA) * cos(RADIANS * LonA)))
 var yA = (earthR * (cos(RADIANS * LatA) * sin(RADIANS * LonA)))
 var zA = (earthR * (sin(RADIANS * LatA)))
-    
+
 var xB = (earthR * (cos(RADIANS * LatB) * cos(RADIANS * LonB)))
 var yB = (earthR * (cos(RADIANS * LatB) * sin(RADIANS * LonB)))
 var zB = (earthR * (sin(RADIANS * LatB)))
-    
+
 var xC = (earthR * (cos(RADIANS * LatC) * cos(RADIANS * LonC)))
 var yC = (earthR * (cos(RADIANS * LatC) * sin(RADIANS * LonC)))
 var zC = (earthR * (sin(RADIANS * LatC)))
-    
+
 var P1 = [xA, yA, zA]
 var P2 = [xB, yB, zB]
 var P3 = [xC, yC, zC]
 
-
-//numpy.linalg.nrom
 func norm (vector: [Double]) -> Double {
     return sqrt(vector.map{$0 * $0}.reduce(0, +))
+}
+
+func dot (vector1: [Double], vector2: [Double]) ->Double{
+    return zip(vector1, vector2).map{$0 * $1}.reduce(0, +)
+}
+
+func cross (vector1 : [Double], vector2: [Double]) -> [Double] {
+    let uvi = vector1[1] * vector2[2] - vector2[1] * vector1[2]
+    let uvj = vector2[0] * vector1[2] - vector1[0] * vector2[2]
+    let uvk = vector1[0] * vector2[1] - vector2[0] * vector1[1]
+    let aux = [uvi, uvj, uvk]
+    return aux
+}
+
+func substract (vector1: [Double], vector2: [Double]) -> [Double]{
+    return zip(vector1, vector2).map{$0 - $1}
+}
+
+func divide (vector: [Double], number: Double) -> [Double]{
+    return vector.map{$0 / number}
+}
+
+func multiply (vector: [Double], number: Double) -> [Double]{
+    return vector.map{$0 * number}
 }
 
 //from wikipedia
 //transform to get circle 1 at origin
 //transform to get circle 2 on x axis
-//var ex = (P2 - P1)
 
+var sub21 = substract(vector1: P2, vector2: P1)
+var sub31 = substract(vector1: P3, vector2: P1)
 
-//    ex = (P2 - P1)/(numpy.linalg.norm(P2 - P1))
-//    i = numpy.dot(ex, P3 - P1)
-//    ey = (P3 - P1 - i*ex)/(numpy.linalg.norm(P3 - P1 - i*ex))
-//    ez = numpy.cross(ex,ey)
-//    d = numpy.linalg.norm(P2 - P1)
-//    j = numpy.dot(ey, P3 - P1)
-//    
+var ex = divide(vector: sub21, number: norm(vector: sub21))
+var i = dot(vector1: ex, vector2: sub31)
+var iex = ex.map{$0 * i}
+var subC = substract(vector1: sub31, vector2: iex)
+var ey = divide(vector: subC, number: norm(vector: subC))
+var ez = cross(vector1: ex, vector2: ey)
+var d = norm(vector: sub21)
+var j = dot(vector1: ey, vector2: sub31)
+
 //    #from wikipedia
 //    #plug and chug using above values
-//    x = (pow(DistA,2) - pow(DistB,2) + pow(d,2))/(2*d)
-//    y = ((pow(DistA,2) - pow(DistC,2) + pow(i,2) + pow(j,2))/(2*j)) - ((i/j)*x)
-//    
+
+
+let x = (pow(DistA,2) - pow(DistB,2) + pow(d,2))/(2*d)
+let y = ((pow(DistA,2) - pow(DistC,2) + pow(i,2) + pow(j,2))/(2*j)) - ((i/j)*x)
+
 //    # only one case shown here
-//    z = numpy.sqrt(pow(DistA,2) - pow(x,2) - pow(y,2))
-//    
+let z = sqrt(pow(DistA,2) - pow(x,2) - pow(y,2))
+//
 //    #triPt is an array with ECEF x,y,z of trilateration point
-//    triPt = P1 + x*ex + y*ey + z*ez
-//    
+let triPt = P1 + multiply(vector: ex, number: x) + multiply(vector: ey, number: y) + multiply(vector: ez, number: z)
+//
 //    #convert back to lat/long from ECEF
 //    #convert to degrees
-//    lat = math.degrees(math.asin(triPt[2] / earthR))
-//    lon = math.degrees(math.atan2(triPt[1],triPt[0]))
-//    
-//    print lat, lon
+let lat = (asin(triPt[2] / earthR))*180/PI
+let lon = (atan2(triPt[1],triPt[0]))*180/PI
+//
+print (lat, lon)
