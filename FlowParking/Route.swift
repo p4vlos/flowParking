643dfd5ad5966a8782664d98c329065032915f4f
p4vlos <nicolaou_paul@hotmail.com>
2017-04-04 14:56:52 +0100

    added the route and green free parking space

---
+++ FlowParking/Route.swift
@@ -0,0 +1,17 @@
+//
+//  Route.swift
+//  FlowParking
+//
+//  Created by Pavlos Nicolaou on 04/04/2017.
+//  Copyright © 2017 Pavlos Nicolaou. All rights reserved.
+//
+
+import MapKit
+
+@objc class Route: NSObject {
+    var coordinates: [CLLocationCoordinate2D]
+    
+    init(edge1: CLLocationCoordinate2D, edge2: CLLocationCoordinate2D, edge3: CLLocationCoordinate2D, edge4: CLLocationCoordinate2D) {
+        self.coordinates = [edge1, edge2, edge3, edge4]
+    }
+}
